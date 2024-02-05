f234419f6eb261e129f230857e8e4841b2b45f93
leonardo rodriguez <leorodr4446@gmail.com>
2024-02-05 15:46:49 -0300

    v1

--- client/src/componentes/Landing.jsx
+++ client/src/componentes/Landing.jsx
@@ -1,12 +1,14 @@
 import {useState } from "react"
 
-import {gridCrad} from "../gridCard"
+//import {gridCrad} from "../gridCard"
+import {users} from "../users"
+
 
 
 export default function Landing(){
     const[local,setlocal]=useState([])
     const[input,setInput]=useState()
-   
+    const[Card,setCard]=useState({})
    
     
 
@@ -17,10 +19,22 @@ export default function Landing(){
         if(name==="ingreso" && value.length===6)
         
 
-        return setlocal([ gridCrad[value[0]][value[1]], gridCrad[value[2]][value[3]] ,gridCrad[value[4]][value[5]] ]      )
-        
+        //return setlocal([ gridCrad[value[0]][value[1]], gridCrad[value[2]][value[3]] ,gridCrad[value[4]][value[5]] ]      )
+        return setlocal([ Card[value[0]][value[1]], Card[value[2]][value[3]] ,Card[value[4]][value[5]] ]      )
 
         
+    }
+    function handleOnChangeselect(e){
+        e.preventDefault(e)
+       
+        const{name,value}=e.target
+
+        
+        return setCard(JSON.parse(value))
+        
+        
+
+
     }
     
     
@@ -28,17 +42,24 @@ export default function Landing(){
     return(
         <div>
             <h3>landing</h3>
-
-
+            <div>
+                <select name="user" id="" onChange={(e)=>{handleOnChangeselect(e)}} autoComplete={false}>
+                    <option value=""></option>
+                    {users.map((e,i)=>{return <option value={JSON.stringify(e.gridCard)} key={i}>{e.name}</option> })}
+                </select>
+            </div>
+            <br />
+            <br />
 
             <div>
-                <input type="text" name="ingreso" value={input}   onChange={(e)=>{handleOnChange(e)}}/>
+                <input type="text" name="ingreso" value={input}   onChange={(e)=>{handleOnChange(e)}} autoComplete='off' />
                 
                 <li htmlFor="">{local}</li>
 
            
 
             </div>
+            
         </div>
     )
 }
---
+++ client/src/users.js
@@ -0,0 +1,8 @@
+import { gridCrad, gridCrad1 } from "./gridCard";
+
+
+export const users=[
+    {name:'leonado_rodriguez',gridCard:gridCrad},
+    {name:'nahir_jara',gridCard:gridCrad1}
+]
+

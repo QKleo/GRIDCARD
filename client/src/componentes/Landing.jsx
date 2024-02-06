8a95afe3aa5345dfa978b36bc6bc1173cede63fb
leonardo rodriguez <leorodr4446@gmail.com>
2024-02-06 08:23:55 -0300

    v1

--- client/src/componentes/Landing.jsx
+++ client/src/componentes/Landing.jsx
@@ -8,7 +8,7 @@ import {users} from "../users"
 export default function Landing(){
     const[local,setlocal]=useState([])
     const[input,setInput]=useState()
-    const[Card,setCard]=useState({})
+    const[Card,setCard]=useState({'blank':'blank'})
    
     
 
@@ -16,21 +16,27 @@ export default function Landing(){
         e.preventDefault(e)
         const{name,value}=e.target
         if(value.length<=6){setInput(value.toUpperCase()   )}
-        if(name==="ingreso" && value.length===6)
+        if(name==="ingreso" && value.length===6 && !Card.blank){
         
 
-        //return setlocal([ gridCrad[value[0]][value[1]], gridCrad[value[2]][value[3]] ,gridCrad[value[4]][value[5]] ]      )
-        return setlocal([ Card[value[0]][value[1]], Card[value[2]][value[3]] ,Card[value[4]][value[5]] ]      )
+        
+        return setlocal([ Card[value[0]][value[1]], Card[value[2]][value[3]] ,Card[value[4]][value[5]] ]      )}
 
         
     }
     function handleOnChangeselect(e){
         e.preventDefault(e)
-       
-        const{name,value}=e.target
-
         
-        return setCard(JSON.parse(value))
+        const{name,value}=e.target
+        if(value!=='blank'){
+        
+        return setCard(JSON.parse(value)) }
+        else{
+             
+            setlocal([])
+            setCard({'blank':value})
+            
+        }
         
         
 
@@ -44,7 +50,7 @@ export default function Landing(){
             <h3>Grid Card</h3>
             <div>
                 <select name="user" id="" onChange={(e)=>{handleOnChangeselect(e)}} autoComplete={false}>
-                    <option value=""></option>
+                    <option value="blank">Selecciona tu nombre</option>
                     {users.map((e,i)=>{return <option value={JSON.stringify(e.gridCard)} key={i}>{e.name}</option> })}
                 </select>
             </div>
@@ -52,11 +58,12 @@ export default function Landing(){
             <br />
 
             <div>
-                <input type="text" name="ingreso" value={input}   onChange={(e)=>{handleOnChange(e)}} autoComplete='off' />
+                <input type="text" name="ingreso" value={input} placeholder="Coordenadas Grid Card"
+                  onChange={(e)=>{handleOnChange(e)}} autoComplete="off" />
                 
                 <li htmlFor="">{local}</li>
 
-           
+            {console.log(Card)}
 
             </div>
             

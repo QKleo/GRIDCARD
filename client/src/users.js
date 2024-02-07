594ec4cea1fd27bbb1cf221ccf5078621fd465bb
leonardo rodriguez <leorodr4446@gmail.com>
2024-02-07 18:31:09 -0300

    v1

--- client/src/users.js
+++ client/src/users.js
@@ -2,7 +2,7 @@ import { gridCrad, gridCrad1 } from "./gridCard";
 
 
 export const users=[
-    {name:'leonado_rodriguez',gridCard:gridCrad},
+    {name:'leonardo_rodriguez',gridCard:gridCrad},
     {name:'nahir_jara',gridCard:gridCrad1}
 ]
 
